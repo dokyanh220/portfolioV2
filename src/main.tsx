ae888f2300a5e369816e5dee25bf8569bda2b4ac
Đỗ Kỳ Anh <164603696+dokyanh220@users.noreply.github.com>
2025-10-27 00:44:28 +0700

    thêm seo

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import 'styles/global.scss';
 import Layout from '@/layout';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from 'pages/home';
 import ProjectPage from '@/pages/project';
 import AboutPage from '@/pages/about';
@@ -13,31 +10,30 @@ import { AppContextProvider } from 'components/context/app.context';
 import '@/i18n';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />
-      },
-      {
-        path: "/project",
-        element: <ProjectPage />,
-      },
-      {
-        path: "/about",
-        element: <AboutPage />,
-      },
-    ]
-  }
-
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: '/project',
+                element: <ProjectPage />,
+            },
+            {
+                path: '/about',
+                element: <AboutPage />,
+            },
+        ],
+    },
 ]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AppContextProvider>
-      <RouterProvider router={router} />
-    </AppContextProvider>
-  </StrictMode>,
-)
+    <StrictMode>
+        <AppContextProvider>
+            <RouterProvider router={router} />
+        </AppContextProvider>
+    </StrictMode>
+);
